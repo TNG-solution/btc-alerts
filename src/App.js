98771a875b13e0ed211fb7f769ee96e02550b83d
Luis Gerardo Huerta Sánchez <luisgerhuerta@gmail.com>
2021-03-26 18:42:59 -0600

    Added margin in App.js

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ function App() {
       <Provider store={store}>
         <Navbar/>
 
-        <div>
+        <div className="m-5">
           <Switch>
             <Route exact path="/" component={Main}/>
             <Route exact path="/bitcoin_price" component={Btc}/>
