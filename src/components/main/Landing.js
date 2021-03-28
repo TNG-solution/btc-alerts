4cfb4ded7c3c78e5a8ae80192870cd69c5a6ad05
Luis Gerardo Huerta Sánchez <luisgerhuerta@gmail.com>
2021-03-28 17:02:41 -0600

    Added animations in landing page and replaced Link by button

--- src/components/main/Landing.js
+++ src/components/main/Landing.js
@@ -1,26 +1,42 @@
 import React from 'react';
 
-import { Link } from 'react-router-dom';
+// Packages
+import { motion } from 'framer-motion';
 
 // Images
 import landing from '../../images/landing-img.svg';
 
 const Landing = () => {
+  const goToBtc = (e) => {
+    e.preventDefault();
+    window.location.href = '/bitcoin_price';
+  }
+
   return ( 
     <div className="row align-items-center vh-100">
-      <div className="col">
+      <motion.div className="col"
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ delay: 0.3, duration: 1.3 }}
+      >
         <h3 className="font-weight-normal">Recibe alertas</h3>
         <h1 className="font-weight-bold" style={{fontSize: "75px"}}>Seguras y confiables</h1>
         <br/>
         <h5 className="font-weight-normal">Conocerás el tiempo indicado para saber cuando vender o comprar bitcoin.</h5>
         <br/><br/><br/>
-        <Link className="btn d-inline rounded-pill" id="start_button" to={"/bitcoin_price"}>
+        <motion.button className="btn d-inline rounded-pill" id="start_button" onClick={goToBtc}
+          whileHover={{ scale: 1.1 }}
+        >
           Comenzar &#8594;
-        </Link>
-      </div>
-      <div className="col">
+        </motion.button>
+      </motion.div>
+      <motion.div className="col"
+        initial={{ y: 200, opacity: 0 }}
+        animate={{ y: 0, opacity: 1 }}
+        transition={{ delay: 0.3, duration: 1.3 }}
+      >
         <img src={landing} alt="btc-alerts" height="550"/>
-      </div>
+      </motion.div>
     </div>
    );
 }
