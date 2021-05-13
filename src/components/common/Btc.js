9ea67f86e91e1716b95452b7dafc509c8513f560
Luis Gerardo Huerta Sánchez <luisgerhuerta@gmail.com>
2021-05-12 20:18:13 -0500

    Added exchange to display bitcoin price in btc component

--- src/components/common/Btc.js
+++ src/components/common/Btc.js
@@ -1,4 +1,8 @@
 import React, { Fragment, useState, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+
+// Redux
+import { get_exchanges } from '../../redux/actions/exchangesActions';
 
 // Material UI
 import { Grid, Card, CardHeader, List, ListItem, ListItemText, Divider, Typography } from '@material-ui/core';
@@ -6,12 +10,26 @@ import { btcStyles } from '../../styles/index';
 
 // Packages
 import TradingViewWidget, { Themes, BarStyles } from 'react-tradingview-widget';
+import { motion } from 'framer-motion';
+
+// Components
+import Exchange from './Exchange';
 
 const Btc = () => {
+  const dispatch = useDispatch();
   const classes = btcStyles();
 
   const [sell, setSell] = useState(false);
   const [buy, setBuy] = useState(false);
+
+  const exchanges = useSelector(state => state.exchanges.exchanges);
+  const loading = useSelector(state => state.exchanges.loading);
+
+  useEffect(() => {
+    const getExchanges = () => dispatch(get_exchanges());
+    getExchanges();
+  }, []);
+
   const onClick = (e) => {
     e.preventDefault();
 
@@ -25,6 +43,23 @@ const Btc = () => {
 
   return (
     <div className={classes.root}>
+      <Grid container direction="row" justify="space-around" alignItems="center" spacing={3}>
+        {exchanges !== undefined
+          ? exchanges.map(item => (
+              <Grid item xs={6} sm={3}>
+                <motion.div
+                  initial={{ y: -200, opacity: 0 }}
+                  animate={{ y: 0, opacity: 1 }}
+                  transition={{ delay: 0.3, duration: 1.3 }}
+                >
+                  <Exchange key={item.name} exchange={item} loading={loading}/>
+                </motion.div>
+              </Grid>
+            ))
+            : null
+          }
+      </Grid>
+      
       <Grid container direction="row" justify="space-around" alignItems="center" spacing={3}>
         <Grid item xs={8}>
           <button type="button" onClick={onClick}>click</button>
