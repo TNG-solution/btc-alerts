b94317749a1d790c6cb5c6527c045a245a961709
Luis Gerardo Huerta Sánchez <luisgerhuerta@gmail.com>
2021-05-12 20:13:15 -0500

    Added alert price test list

--- src/components/common/Btc.js
+++ src/components/common/Btc.js
@@ -1,7 +1,7 @@
-import React from 'react';
+import React, { Fragment, useState, useEffect } from 'react';
 
 // Material UI
-import { Grid, Card, CardHeader, List, ListItem, ListItemText, Divider } from '@material-ui/core';
+import { Grid, Card, CardHeader, List, ListItem, ListItemText, Divider, Typography } from '@material-ui/core';
 import { btcStyles } from '../../styles/index';
 
 // Packages
@@ -10,10 +10,24 @@ import TradingViewWidget, { Themes, BarStyles } from 'react-tradingview-widget';
 const Btc = () => {
   const classes = btcStyles();
 
+  const [sell, setSell] = useState(false);
+  const [buy, setBuy] = useState(false);
+  const onClick = (e) => {
+    e.preventDefault();
+
+    setBuy(true); 
+    setSell(true); 
+    setTimeout(() => {
+      setBuy(false); 
+      setSell(false);
+    }, 4700);
+  }
+
   return (
     <div className={classes.root}>
       <Grid container direction="row" justify="space-around" alignItems="center" spacing={3}>
         <Grid item xs={8}>
+          <button type="button" onClick={onClick}>click</button>
           <Card className={classes.card}>
             <TradingViewWidget
               symbol="BITBAY:BTCUSD"
@@ -31,13 +45,51 @@ const Btc = () => {
           <Card className={classes.alertsCard}>
             <CardHeader title="Alertas"/>
             <List>
-              <ListItem className={classes.alertSuccess}>
-                <ListItemText primary="¡Es hora de comprar!" secondary="Ahora"/>
-              </ListItem>
-              <Divider />
-              <ListItem className={classes.alertDanger}>
-                <ListItemText primary="¡Es hora de vender!" secondary="Ahora"/>
-              </ListItem>
+              {buy
+                ? <ListItem className="alert-buy">
+                    <ListItemText
+                      primary={
+                        <Fragment>
+                          <Typography variant="h5" className={classes.alert}>¡Es hora de comprar!</Typography>
+                        </Fragment>
+                      }
+                    />
+                  </ListItem>
+                : <ListItem className={classes.alertBuy}>
+                    <ListItemText primary="Compra Bitcoin" 
+                      secondary={
+                        <Fragment>
+                          <Typography component="span" variant="body2" className={classes.text}>
+                            Puedes hacerlo desde tu wallet preferida.
+                          </Typography>
+                        </Fragment>
+                      }
+                    />
+                  </ListItem>
+              }
+              <Divider/>
+              {sell
+                ? <ListItem className="alert-sell">
+                    <ListItemText
+                      primary={
+                        <Fragment>
+                          <Typography variant="h5" className={classes.alert}>¡Es hora de vender!</Typography>
+                        </Fragment>
+                      }
+                    />
+                  </ListItem>
+                : <ListItem className={classes.alertSell}>
+                    <ListItemText primary="Vende Bitcoin" 
+                      secondary={
+                        <Fragment>
+                          <Typography component="span" variant="body2" className={classes.text}>
+                            Puedes hacerlo desde tu wallet preferida.
+                          </Typography>
+                        </Fragment>
+                      }
+                    />
+                  </ListItem>
+              }
             </List>
           </Card>
         </Grid>
